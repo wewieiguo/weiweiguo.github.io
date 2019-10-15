68371101ef91169ae4f9cb9d511ed97787060a46
‘guoweiwei <ema>
2019-10-15 19:59:41 +0800

    首次提交

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Demo from "./demo/Demo"
+function App() {
+  return (
+    <div className="App">
+      <Demo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/demo/Demo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import styles from "./Demo.module.less"
+export default class Demo extends Component {
+    render() {
+        return (
+            <div className={`${styles["container"]}`}>
+                11111
+            </div>
+        )
+    }
+}
